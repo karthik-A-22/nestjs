2648d038868d3b7c6d038f6601e3ea31d09d83c1
karthik-A <karthik.a@fortunesoftit.com>
2025-07-04 10:33:03 +0530

    lesson03

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [role];
+  }
+
+  @Get('interns')
+  findInterns() {
+    return [];
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return { user, message: 'User created successfully.' };
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUser: {}) {
+    return {
+      data: { id, ...updateUser },
+      message: 'User updated successfully.',
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param(':id') id: string) {
+    return { id, message: 'User deleted' };
+  }
+}
